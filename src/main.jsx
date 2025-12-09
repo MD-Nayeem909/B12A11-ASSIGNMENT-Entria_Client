5cf5f49e45324dd16ae2207499306763f0b95fc3
MD-Nayeem909 <mdnayeemuddin909@gmail.com>
2025-12-09 21:43:48 +0600

    Completely redesign Footer with modern layout, social icons, and improved styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import router from './routes/router.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import router from "./routes/router.jsx";
+import AuthProvider from "./Providers/AuthProvider.jsx";
+import ThemeProvider from "./Providers/ThemeProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </AuthProvider>
+  </StrictMode>
+);
