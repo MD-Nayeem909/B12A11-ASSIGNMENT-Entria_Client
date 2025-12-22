530ead54c05df4dcf82cea91a7956cecc7045619
MD-Nayeem909 <mdnayeemuddin909@gmail.com>
2025-12-22 14:07:50 +0600

    Update LoadingSpinner layout and sizing

--- src/components/common/LoadingSpinner.jsx
+++ src/components/common/LoadingSpinner.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center min-h-[calc(100vh-285px)]">
+    <div className="flex justify-center items-center min-h-[calc(100vh-80px)]">
       <DotLottieReact
         src="https://lottie.host/e2cd019f-e675-4b2a-a1f7-d8cfbbf1e863/HgUeSAvHvm.lottie"
         loop
         autoplay
-        className=""
+        style={{ width: "300px", height: "300px" }}
       />
     </div>
   );
