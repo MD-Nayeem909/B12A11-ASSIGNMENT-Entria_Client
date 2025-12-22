35ae55a47052076515af075c194b7e3433b4fd4d
MD-Nayeem909 <mdnayeemuddin909@gmail.com>
2025-12-22 23:46:39 +0600

    Remove logo image from Logo component

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import logo from "../../assets/logo.png";
 import { Link } from "react-router";
 
 const Logo = () => {
   return (
     <Link to="/">
       <div className="flex items-center gap-2">
-        <img src={logo} alt="" className="h-8" />
         <h1 className="font-bold text-2xl">ENTRIA</h1>
       </div>
     </Link>
