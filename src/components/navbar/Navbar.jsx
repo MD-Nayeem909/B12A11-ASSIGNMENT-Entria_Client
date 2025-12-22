6e36098329e8f0da93ed2af31aba5b2b2ec51c19
MD-Nayeem909 <mdnayeemuddin909@gmail.com>
2025-12-22 23:38:46 +0600

    Add .jsx extension to Logo component import in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "../common/Button";
 import { NavLink } from "react-router";
-import Logo from "../common/logo";
+import Logo from "../common/logo.jsx";
 import { useTheme } from "../../Providers/ThemeProvider";
 import useAuth from "../../hooks/useAuth";
 import UserProfileDropdown from "../Profile/UserProfileDropdown";
